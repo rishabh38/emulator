c3048afb66d558ee04de6da59f6f866f0f541c2b
rishabh <onrishabh@gmail.com>
2019-02-13 05:05:25 +0530

    added string_utility

---
+++ executer/include/string_utility.h
@@ -0,0 +1,13 @@
+#ifndef EXECUTER_INCLUDE_STRING_UTILITY_H
+#define EXECUTER_INCLUDE_STRING_UTILITY_H
+
+#include <string>
+#include <utility>
+
+bool skip_char (const std::string&, char, size_t&);
+std::pair<uint64_t, uint64_t> get_range (const std::string&);
+std::pair<uint64_t, uint64_t> get_rangei (const std::string&, size_t&);
+std::string get_bitS (const std::string&, size_t&);
+std::string get_procedure (const std::string&, size_t&);
+
+#endif
---
+++ executer/string_utility.cc
@@ -0,0 +1,131 @@
+#include "executer/include/string_utility.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string get_bitS (const string &str, size_t &index) {
+  size_t init_index = index;
+  size_t str_length = str.size();
+  string bitS;
+
+  while (index < str_length) {
+    if ((unsigned)(str[index] ^ '0') > 1) {
+      break;
+    }
+
+    bitS.push_back (str[index]);
+    index++;
+  }
+
+  return bitS;
+}
+
+bool skip_char (const string &str, char skipchar, size_t &index) {
+  size_t str_length = str.size();
+  bool success = 1;
+
+  if (!str_length) {
+    return 0;
+  }
+
+  for (; index < str_length; index++) {
+    if (!isspace (str[index])) {
+     if (str[index] == skipchar) {
+        index++;
+        break;
+      }
+
+      success = 0;
+      break;
+    }
+  }
+
+  return success;
+}
+
+pair<uint64_t, uint64_t> get_rangei (const string &rnge_string, size_t &i) {
+  pair<uint64_t, uint64_t> error_return_value = make_pair (0,0);
+  size_t rnge_string_length = rnge_string.size();
+
+  if (!rnge_string_length) {
+    cerr << "get_rangei: empty range_string" << endl;
+    return error_return_value;
+  }
+
+  bool success = 1;
+  success = skip_char (rnge_string, '[', i);
+
+  if (!success) {
+    cerr << "get_rangei: invalid range string" << endl;
+    return error_return_value;
+  }
+
+  size_t processed_chars;
+  unsigned num1 = stoul (rnge_string.substr (i), &processed_chars);
+  i += processed_chars;
+  success = skip_char (rnge_string, '-', i);
+
+  if (!success) {
+    cerr << "get_rangei: invalid range string" << endl;
+    return error_return_value;
+  }
+
+  unsigned num2 = stoul (rnge_string.substr (i), &processed_chars);
+  i += processed_chars;
+  success = skip_char (rnge_string, ']', i);
+
+  if (!success) {
+    cerr << "get_rangei: invalid range string" << endl;
+    return error_return_value;
+  }
+
+  return make_pair (num1, num2);
+}
+
+pair<uint64_t, uint64_t> get_range (const string &str) {
+  size_t index = 0;
+  return get_rangei (str, index);
+}
+
+
+string get_procedure (const string &statement, size_t& index) {
+  size_t statement_length = statement.size();
+
+  if (!statement_length) {
+    cerr << "separate_subprocedure: empty statement" << endl;
+    return "";
+  }
+
+  while (index < statement_length && isspace (statement[index])) {
+    index++;
+  }
+
+  if (statement[index] != '(') {
+    cerr << "separate_subprocedure: indicated position in "
+         << "statement doesn't represent subprocedure"
+         << endl;
+    return "";
+  }
+
+  size_t index_beg = index;
+  int level = 0;
+
+  for (; index < statement_length; index++) {
+    if (statement[index] == '(') {
+      level++;
+    }
+    else if (statement[index] == ')') {
+      level--;
+    }
+
+    if (level <= 0) {
+      break;
+    }
+  }
+
+  string subprocedure = statement.substr (index_beg,
+                                          index - index_beg + 1);
+  return subprocedure;
+}
