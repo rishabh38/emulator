23ab5b6668ff877d8cd67c55d8ab4000d134de80
rishabh <onrishabh@gmail.com>
2019-02-17 03:58:48 +0530

    added initializer

---
+++ executer/include/initializer.h
@@ -0,0 +1,17 @@
+#ifndef EXECUTER_INCLUDE_INITIALIZER
+#define EXECUTER_INCLUDE_INITIALIZER
+
+#include <cinttypes>
+#include <string>
+#include <utility>
+#include <vector>
+
+using num_pair = std::pair <uint64_t, uint64_t>;
+
+bool initialize_regmem (const std::string&);
+bool initialize_operators();
+bool initialize_opcode_statement_map ();
+
+bool set_regmem_size (std::pair <num_pair, num_pair>);
+bool init_reg_aliases (std::vector<std::vector<std::string>>);
+#endif
