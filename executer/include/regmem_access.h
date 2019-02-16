d63725d740a3781fc5aa067b9dbb6a299fa3640e
rishabh <onrishabh@gmail.com>
2019-02-17 05:20:36 +0530

    made alias_to_reg_bitS public

--- executer/include/regmem_access.h
+++ executer/include/regmem_access.h
@@ -10,6 +10,7 @@ bool add_reg_alias (std::string alias, std::string bitS);
 bool init_reg (uint16_t total_reg, uint16_t width);
 bool init_mem (uint32_t size, uint16_t width);
 bool insert_reg_bitS (std::string reg_bitS, uint16_t regIndex);
+std::string alias_to_reg_bitS (std::string alias);
 std::string get_reg_value (std::string reg_bitS);
 std::string get_mem_value (std::string mem_bitS);
 bool insert_reg_value (std::string reg_bitS, std::string bitS);
