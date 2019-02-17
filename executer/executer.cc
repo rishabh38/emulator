e755c6d48f4488e170cad5f3f2539353bfaec696
rishabh <onrishabh@gmail.com>
2019-02-17 07:28:14 +0530

    added executer

---
+++ executer/executer.cc
@@ -0,0 +1,47 @@
+#include "executer/include/executer.h"
+
+#include "executer/include/statement_reader.h"
+#include "executer/include/regmem_access.h"
+#include "executer/include/opcode_matcher.h"
+
+#include <iostream>
+
+using namespace std;
+
+bool do_all_match (const string &str, const char ch) {
+  bool success = true;
+
+  for (auto a : str) {
+    if (a != ch) {
+      success = false;
+      break;
+    }
+  }
+
+  return success;
+}
+  
+
+void execute () {
+  string pc_alias = "PC";
+  string pc_data = get_reg_value (pc_alias);
+  
+  while (!do_all_match (pc_data, '0')) { 
+    string opcode = get_mem_value (pc_data);
+
+    if (!opcode.size()) {
+      break;
+    }
+
+    vector<string> statements = get_statements (opcode);
+
+    if (!statements.size()) {
+      break;
+    }
+  
+    for (auto statement : statements) {
+      execute_statement (statement, opcode);
+    }
+  }
+}
+  
---
+++ executer/include/executer.h
@@ -0,0 +1,11 @@
+#ifndef EXECUTER_INCLUDE_EXECUTER_H
+#define EXECUTER_INCLUDE_EXECUTER_H
+
+#include "executer/include/statement_reader.h"
+#include "executer/include/regmem_access.h"
+#include "executer/include/opcode_matcher.h"
+
+ 
+void execute ();
+
+#endif
