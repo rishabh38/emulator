2a041736a25c1f82d77b0d886e152c623a54b2b7
rishabh <onrishabh@gmail.com>
2019-02-17 06:04:46 +0530

    renamed parameter

--- executer/include/regmem_access.h
+++ executer/include/regmem_access.h
@@ -11,7 +11,7 @@ bool init_reg (uint16_t total_reg, uint16_t width);
 bool init_mem (uint32_t size, uint16_t width);
 bool insert_reg_bitS (std::string reg_bitS, uint16_t regIndex);
 std::string alias_to_reg_bitS (std::string alias);
-std::string get_reg_value (std::string reg_bitS);
+std::string get_reg_value (std::string reg);
 std::string get_mem_value (std::string mem_bitS);
 bool insert_reg_value (std::string reg_bitS, std::string bitS);
 bool insert_mem_value (std::string mem_bitS, std::string bitS);
